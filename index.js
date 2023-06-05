2fbbfdcf8da069998342fc4a6970bc26ed5c8af1
jojopedrorodrigues <jlabssistem@gmail.com>
2023-06-05 16:46:50 -0300

    GravandoDadosUsandoExpressSqlite

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 const fs = require('fs');
-const http = require('http');
-const { URLSearchParams } = require('url');
-const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('dados.db');
-const readFile = fs.readFileSync('page.html', 'utf8');
-const saveFile = fs.readFileSync('savePage.html', 'utf8');
-db.run('CREATE TABLE IF NOT EXISTS dados (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, email VARCHAR, number VARCHAR)');
+const express = require('express');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    if (req.method === 'POST') {
-        let body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-            const formDataInfo = new URLSearchParams(body);
-            const nome = formDataInfo.get('nome');
-            const email = formDataInfo.get('email');
-            const number = formDataInfo.get('number');
-            db.run('INSERT INTO dados (nome, email, number) VALUES (?, ?, ?)', [nome, email, number], function (err, result) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    res.end(saveFile);
-                }
-            });
-        });
-    } else if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end(readFile);
-    }
+const app = express();
+const db = new sqlite3.Database('newdata.db');
+
+const pageUm = fs.readFileSync('page.html', 'utf8');
+const callBackPage = fs.readFileSync('savePage.html', 'utf8');
+
+db.run('CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, email VARCHAR)');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'page.html'));
 });
 
-server.listen(3000, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Servidor Online na porta 3000');
-    }
+app.post('/', function (req, res) {
+    const nome = req.body.nome;
+    const email = req.body.email;
+
+    db.run('INSERT INTO usuarios (name, email) VALUES (?, ?)', [nome, email], function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.sendFile(path.join(__dirname, 'savePage.html'));
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Server running at http://localhost:3000/');
 });
